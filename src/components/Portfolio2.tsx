e3cfe511d5dd6f0557da3acc8dd62842ef870105
panaddaa04 <panadda.do@ku.th>
2025-03-05 12:45:45 +0700

    first commit

---
+++ src/components/Portfolio2.tsx
@@ -0,0 +1,273 @@
+import React from "react";
+
+export default function Portfolio2() {
+  return (
+    <div>
+      <>
+        {/* full Title */}
+        <div className="full-title">
+          <div className="container">
+            {/* Page Heading/Breadcrumbs */}
+            <h1 className="mt-4 mb-3">
+              การจัดกิจกรรมเพื่อการพัฒนาองค์การภาครัฐ<small>Subheading</small>
+            </h1>
+          </div>
+        </div>
+        {/* Page Content */}
+        <div className="container">
+          <div className="breadcrumb-main">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <a href="index.html">หน้าหลัก</a>
+              </li>
+              <li className="breadcrumb-item active">
+                การจัดกิจกรรมเพื่อการพัฒนาองค์การภาครัฐ
+              </li>
+            </ol>
+          </div>
+          <div className="row">
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p4.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/organization intervention/organization_intervention-1-41.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การบริหารผลงาน
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การบริหารผลงาน คือ กระบวนการบริหารเชิงยุทธศาสตร์
+                    ที่มุ่งทำให้ทั้งองค์การและบุคลากร
+                    ทำงานบรรลุผลสำเร็จตามเป้าหมาย
+                    โดยการปรับปรุงผลการปฏิบัติงานขององค์การ หน่วยงาน
+                    และพนักงานแต่ละคน และพัฒนาขีดความสามารถของพนักงานในทุกระดับ
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p7.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/organization_ intervention/organization- intervention-42-75.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การปรับโครงสร้างองค์การ
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การปรับโครงสร้างองค์กรหมายถึงกระบวนการปรับโครงสร้างลำดับชั้น
+                    โครงสร้างภายใน หรือขั้นตอนการดำเนินงานของบริษัท บริษัทต่างๆ
+                    ดำเนินการปรับโครงสร้างเพื่อบรรลุเป้าหมายบางประการ เช่น
+                    เพื่อให้สามารถแข่งขันได้มากขึ้นหรือตอบสนองต่อการเปลี่ยนแปลงในตลาด
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p6.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/organization_ intervention/organization- intervention-76-111.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การมีส่วนร่วมของพนักงาน
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การมีส่วนร่วมของพนักงานคือการได้รับรวมเป็นส่วนหนึ่งของทีมอย่างเต็มตัว
+                    มุ่งเน้นไปที่เป้าหมายที่ชัดเจน ได้รับความไว้วางใจและอำนาจ
+                    ได้รับการตอบรับเชิงบวกและสร้างสรรค์อย่างสม่ำเสมอ
+                    ได้รับการสนับสนุนในการพัฒนาทักษะใหม่ๆ
+                    ได้รับการขอบคุณและได้รับการยอมรับสำหรับความสำเร็จ
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p8.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/organization_ intervention/organization- intervention-112-142.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      กลยุทธ์การแข่งขันและการร่วมมือ
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    กลยุทธ์การมีส่วนร่วมของพนักงานเป็นกุญแจสำคัญในการสร้างทีมที่ให้ความสำคัญกับสิ่งอื่นๆ
+                    มากกว่าเงินเดือน เหตุใดจึงเป็นเช่นนั้น?
+                    เพราะพนักงานที่มีส่วนร่วมจะทุ่มเทอย่างเต็มที่กับบทบาทของตนเอง
+                    ได้รับพลังจากงานที่ทำ
+                    และขับเคลื่อนด้วยความปรารถนาอย่างแท้จริงที่จะเห็นองค์กรของคุณเจริญรุ่งเรือง
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p10.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/techostructure/ilovepdf_split-range (1)/technostructure-1-32.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การออกแบบงาน
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การออกแบบงาน
+                    เป็นเอกสารที่เขียนอย่างเป็นทางการซึ่งระบุความรับผิดชอบของงาน
+                    คุณสมบัติที่จำเป็น และหน้าที่
+                    โดยไม่นับรายละเอียดเกี่ยวกับค่าตอบแทนและสวัสดิการ
+                    เอกสารดังกล่าวจะจัดโครงสร้างและจัดระเบียบบทบาทงานต่างๆ
+                    ภายในองค์กรอย่างมีกลยุทธ์ ประกอบด้วยแนวทางต่างๆ
+                    ที่มุ่งเน้นที่การเพิ่มประสิทธิภาพ ความพึงพอใจของพนักงาน
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p11.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/techostructure/ilovepdf_split-range (1)/technostructure-33-54.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การ Coaching
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การโค้ช
+                    Coachingคือกระบวนการทำงานร่วมกันระหว่างโค้ชซึ่งเป็นเพื่อนชวนคิด
+                    หรือปลดล๊อคบางอย่างในตัวผู้รับการโค้ช (Coachee อ่านว่า
+                    โค้ชชี่) มีศักยภาพสูงขึ้น หรือมีความสุขอย่างที่เขาต้องการ
+                    ผ่านวิธีการและเครื่องมือต่างๆ เพื่อให้ผู้รับการโค้ช
+                    ได้เรียนรู้ ตระหนักในตัวเองและเปลี่ยนแปลง
+                    และลงมือทำด้วยความคิด ความถนัด{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p9.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/techostructure/ilovepdf_split-range (1)/technostructure-55-77.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การสร้างทีม
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การสร้างทีมงาน หมายถึง
+                    ความพยายามที่จะเข้าใจพฤติกรรมของคนที่ต้องมาปฏิบัติงานร่วมกัน
+                    และทำให้กลุ่มคนเหล่านั้นสามารถเรียนรู้ วิเคราะห์ปัญหา
+                    และหาทางออกในการปฏิบัติงานร่วมกัน
+                    เพื่อให้งานบรรลุเป้าหมายที่วางไว้ ดังนั้น
+                    หน่วยงานหรือองค์กรที่จะประสบความสำเร็จ
+                    ต้องมีการสร้างทีมที่มีความสามัคคี
+                    และมีเป้าหมายการทำงานเหมือนกัน
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 portfolio-item">
+              <div className="card h-100">
+                <a href="#">
+                  <img className="card-img-top" src="images/p12.jpg" alt="" />
+                </a>
+                <div className="card-body">
+                  <h4 className="card-title">
+                    <a
+                      href="assets/techostructure/ilovepdf_split-range (1)/technostructure-78-123.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      การเปลี่ยนวัฒนธรรมองค์กร
+                    </a>
+                  </h4>
+                  <p className="card-text">
+                    การเปลี่ยนแปลงวัฒนธรรมองค์กรคือกระบวนการพัฒนาวัฒนธรรมองค์กรในรูปแบบที่แท้จริงซึ่งดึงดูดผู้คนและสร้างแรงบันดาลใจให้ทีมทำงานได้อย่างเต็มประสิทธิภาพ
+                    เป็นเรื่องของการกำหนดและปลูกฝังพฤติกรรมที่ต้องการและปรับปรุงประสบการณ์ของพนักงานเพื่อเพิ่มการมีส่วนร่วม
+                    การรักษาพนักงาน ขวัญกำลังใจ
+                    และท้ายที่สุดช่วยให้คุณบรรลุวิสัยวิสัยทัศน์
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* /.row */}
+          <div className="pagination_bar">
+            {/* Pagination */}
+            <ul className="pagination justify-content-center">
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">«</span>
+                  <span className="sr-only">Previous</span>
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  2
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  3
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">»</span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* /.container */}
+      </>
+    </div>
+  );
+}
